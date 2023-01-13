603340393008848957e5e1a603fbe83fb6d438a0
barry006 <120657043+barry006@users.noreply.github.com>
2023-01-13 23:25:25 +0100

    Update MeshColliderUpdate.cs

--- Assets/Scripts/important/MeshColliderUpdate.cs
+++ Assets/Scripts/important/MeshColliderUpdate.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class MeshColliderUpdate : MonoBehaviour
 {
-   // private float time = 0;
     SkinnedMeshRenderer skinnedMeshRenderer;
-    //MeshColliderUpdate colliderMesh;
     private Mesh bakedMesh;
-   
-    void Start()
-    {
-        
-        //colliderMesh = GetComponent<MeshCollider>();
-    }
 
     private void Awake()
     {
@@ -26,31 +18,15 @@ public class MeshColliderUpdate : MonoBehaviour
     }
     void Update()
     {
-       // time += Time.deltaTime;
-       // if (time >= 0)
-      //  {
-           // time = 0;
             UpdateCollider();
-       // }
+
     }
 
     public void UpdateCollider()
-    {
-        
-            
-            
+    { 
             skinnedMeshRenderer.BakeMesh(bakedMesh);
             GetComponent<MeshCollider>().sharedMesh = null;
-            GetComponent<MeshCollider>().sharedMesh = bakedMesh;
-            
-        
-            //Destroy(colliderMesh);
-           
-
-
+            GetComponent<MeshCollider>().sharedMesh = bakedMesh;           
+            //Destroy(colliderMesh);           
     }
 }
-
-
-// You must use on the *SkinnedMesh* , and puthe the *mesh collider* too on the (( Mesh Avatar )) ... i think you got the ideia.
-//Vous devez utiliser sur le SkinnedMesh , et mettre le collisionneur de maillage aussi sur le (( Mesh Avatar )) ... je pense que vous avez l'ideia.
